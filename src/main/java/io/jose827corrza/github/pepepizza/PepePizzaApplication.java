889aa6205f8824cff02ca601d272c009c6494477
Jose Corredor <jose96corrza@gmail.com>
2023-10-23 14:50:30 -0500

    :rocket: Initial commit, basic files, connected to Postgres DB which is locally running on a docker-compose

---
+++ src/main/java/io/jose827corrza/github/pepepizza/PepePizzaApplication.java
@@ -0,0 +1,13 @@
+package io.jose827corrza.github.pepepizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PepePizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PepePizzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/jose827corrza/github/pepepizza/PepePizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jose827corrza.github.pepepizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PepePizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
