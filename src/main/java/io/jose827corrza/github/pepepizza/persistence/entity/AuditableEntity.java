25707d548534d9e115c1230fbd1095b84b4c2468
Jose Corredor <jose96corrza@gmail.com>
2023-10-26 16:54:22 -0500

    Basic auditoring, also a 'parent' class created which allow us to extends on the other entities and use it easily

---
+++ src/main/java/io/jose827corrza/github/pepepizza/persistence/entity/AuditableEntity.java
@@ -0,0 +1,23 @@
+package io.jose827corrza.github.pepepizza.persistence.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public class AuditableEntity {
+    // For auditing
+    @Column(name = "created_at")
+    @CreatedDate
+    @JsonIgnore
+    private LocalDateTime createdDate;
+
+    @Column(name = "updated_at")
+    @LastModifiedDate
+    @JsonIgnore
+    private LocalDateTime updatedDate;
+}
--- src/main/java/io/jose827corrza/github/pepepizza/persistence/entity/PizzaEntity.java
+++ src/main/java/io/jose827corrza/github/pepepizza/persistence/entity/PizzaEntity.java
@@ -1,16 +1,23 @@
 package io.jose827corrza.github.pepepizza.persistence.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "pizzas")
+@EntityListeners(AuditingEntityListener.class)// Enable auditing for the entity
 @Getter
 @Setter
 @NoArgsConstructor // Avoid using @Data, bad practice to use in entities
-public class PizzaEntity {
+public class PizzaEntity extends AuditableEntity{ // extends ang gets the Column of that class
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) //Here we could select uuid instead of a single integer
@@ -47,4 +54,6 @@ public class PizzaEntity {
                 ", available=" + available +
                 '}';
     }
+
+
 }
